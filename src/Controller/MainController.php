0dbdba1c8b8b169033001cca71f499e9fb200dc2
oumamem <oumamem@gmail.com>
2021-01-22 19:44:25 +0100

    calendar

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="main")
+     * @Route("/calendrier", name="main")
      */
     public function index(CalendarRepository $calendar)
     {
