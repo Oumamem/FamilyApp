19f46e5f2e77bb2fccadcb9029bb88059c279fcf
Oumamem <oumamem@gmail.com>
2021-01-23 11:19:10 +0100

    security

--- src/Entity/Famille.php
+++ src/Entity/Famille.php
@@ -7,6 +7,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+
+
 /**
  * @ORM\Entity(repositoryClass=FamilleRepository::class)
  */
@@ -83,6 +85,9 @@ class Famille
 
         return $this;
     }
+
+
+
 
     /**
      * @return Collection|Membre[]
